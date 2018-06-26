0e12b3b36978585e768a57dc1903363caa9f348e
Tiago Quintino <tiago.quintino@ecmwf.int>
2018-06-26 10:41:45 +0200

    Python 3 and python only connector

--- share/metkit/paramid.py
+++ share/metkit/paramid.py
@@ -2,14 +2,14 @@
 
 import psycopg2
 import yaml
-import MySQLdb
+import mysql.connector
 import sys
 import re
 
-gdb = MySQLdb.connect("grib-param-db-prod.ecmwf.int",
-                      "ecmwf_ro",
-                      "ecmwf_ro",
-                      "param")
+gdb = mysql.connector.connect(host="grib-param-db-prod.ecmwf.int",
+                     user="ecmwf_ro",
+                     password="ecmwf_ro",
+                     database="param")
 
 
 params = sys.argv[1:]
@@ -22,19 +22,19 @@ values = []
 
 for p in params:
     try:
-       a, b = p.split(".")
-       a, b = int(a), int(b)
-       if b == 128:
-          b = 0
-       p = str(b*1000 + a)
+      a, b = p.split(".")
+      a, b = int(a), int(b)
+      if b == 128:
+        b = 0
+      p = str(b*1000 + a)
     except:
-	   pass
+      pass
 
     try:
-       int(p)
+      int(p)
     except:
-       what="shortName"
-       p="'%s'" % (p,)
+      what="shortName"
+      p="'%s'" % (p,)
 
 
     values.append(p)
@@ -44,11 +44,11 @@ for p in params:
 
 gcursor = gdb.cursor()
 gcursor.execute("""
-SELECT param.id, param.shortName, param.name, units.name 
-FROM param, units 
+SELECT param.id, param.shortName, param.name, units.name
+FROM param, units
 WHERE param.%s IN (%s) AND units.id = units_id
 """ % (what, ",".join(values)))
 
 
 for n in gcursor.fetchall():
-    print "%s,%s,%s,%s" % tuple(n)
+    print("{},{},{},{}".format(tuple(n)))
