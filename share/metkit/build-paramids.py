6475fcfdae12aa9d140259ebe95872117e5c743c
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2018-07-20 07:50:47 +0100

    params

--- share/metkit/build-paramids.py
+++ share/metkit/build-paramids.py
@@ -1,38 +1,52 @@
 #!/usr/bin/env python
-
+import MySQLdb
 import yaml
-import mysql.connector
+
+
+db = MySQLdb.connect("grib-param-db-prod.ecmwf.int",
+                      "ecmwf_ro",
+                      "ecmwf_ro",
+                      "param")
+
+prodgen = {}
+
+with open("prodgen.yaml") as f:
+    PRODGEN = yaml.load(f.read())
 
 with open("paramids.yaml") as f:
     PARAMSIDS = yaml.load(f.read())
-
-db = mysql.connector.connect(host="grib-param-db-prod.ecmwf.int",
-                     user="ecmwf_ro",
-                     password="ecmwf_ro",
-                     database="param")
 
 cursor = db.cursor()
 
 cursor.execute("select * from param")
 
 for data in cursor.fetchall():
-    paramid, abbr, longname = data[0], data[1], data[2]
+    paramid, abbr, longname = int(data[0]), data[1].lower(), data[2].lower()
 
     if abbr == '~':
         continue
 
+    entry = [abbr, longname]
+
+    if paramid in PRODGEN:
+      pass
+  
+    entry = tuple(entry)
+
     if paramid in PARAMSIDS:
-        pass
+       before = tuple(PARAMSIDS[paramid]) 
+       if before != entry:
+           print("WARNING! updated paramid: {},  {} => {}".format(paramid, before, entry))
+           PARAMSIDS[paramid] = list(entry)
     else:
-        print("new paramid: {} {} {}".format(int(paramid), abbr, longname.lower()))
-        PARAMSIDS[paramid] = [abbr, longname]
+        print("new paramid: {} ".format(paramid, entry))
+        PARAMSIDS[paramid] = list(entry)
 
 cursor.close()
 db.close()
 
-X = {}
-for k, v in PARAMSIDS.items():
-    X[int(k)] = v
+with open("prodgen.yaml", "w") as f:
+    f.write(yaml.safe_dump(PRODGEN, default_flow_style=False))
 
 with open("paramids.yaml", "w") as f:
-    f.write(yaml.dump(X, default_flow_style=False))
+    f.write(yaml.safe_dump(PARAMSIDS, default_flow_style=False))
