c561973d52af332d2c87b3d35121e69de6dd1bae
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2018-07-21 09:48:59 +0100

    params

---
+++ share/metkit/check-params-yaml.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import MySQLdb
+import yaml
+import re
+import os
+
+
+db = MySQLdb.connect("grib-param-db-prod.ecmwf.int",
+                     "ecmwf_ro",
+                     "ecmwf_ro",
+                     "param")
+
+
+cursor = db.cursor()
+
+
+with open("params.yaml") as f:
+   PARAMS = yaml.load(f.read())
+
+for entry in PARAMS:
+    when, parms = entry
+    for p in parms:
+       cursor.execute("select count(*) from param where id=%s" % p)
+       for data in cursor.fetchall(): 
+           if data[0] == 0:
+              print("%s: %s is not in param DB" % (when, p))
