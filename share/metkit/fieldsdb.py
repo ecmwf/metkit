f527435c2efbf120bc60854090bafa126da0e70f
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2018-11-19 16:24:03 +0000

    fieldsdb

--- share/metkit/fieldsdb.py
+++ share/metkit/fieldsdb.py
@@ -21,18 +21,49 @@ for stream, type, levtype in db.execute(TYPES):
 
 Q = {}
 for k, v in sorted(P.items()):
-    if k[2] == '':
-        d = dict(stream=k[0], type=k[1])
-    else:
-        d = dict(stream=k[0], type=k[1], levtype=k[2])
-
+    d = dict(stream=k[0], type=k[1], levtype=k[2])
     v = tuple(v)
     Q.setdefault(v, [])
     Q[v].append(d)
 
 Y = []
 
+
+def merge(kinds):
+    streams = set()
+    types = set()
+    levtypes = set()
+
+    for k in kinds:
+        streams.add(k[0])
+        types.add(k[1])
+        levtypes.add(k[2])
+
+    streams = sorted(streams)
+    types = sorted(types)
+    levtypes = sorted(levtypes)
+
+    d = {}
+    if len(streams) == 1:
+        d['stream'] = streams[0]
+    else:
+        d['stream'] = streams
+
+    if len(types) == 1:
+        d['type'] = types[0]
+    else:
+        d['type'] = types
+
+    if len(levtypes) == 1:
+        d['levtype'] = levtypes[0]
+    else:
+        d['levtype'] = levtypes
+
+    return d
+
+
 for v, k in Q.items():
+    k = merge(k)
     Y.append([k, v])
 
 print(yaml.safe_dump(Y, default_flow_style=False))
