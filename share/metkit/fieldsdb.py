520acc5e32e307cc84292d40609047d4355d0e10
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2018-11-19 14:56:06 +0000

    fieldsdb

--- share/metkit/fieldsdb.py
+++ share/metkit/fieldsdb.py
@@ -1,11 +1,9 @@
 from sqlalchemy import create_engine, text
 
 db = create_engine("postgres://ecmwf_ro:ecmwf_ro@db-products-dev-00.ecmwf.int/products")
-TYPES = text("select distinct(stream,type,levtype) from fields")
-PARAMS = text("select distinct(param) from fields where stream=? and type=? and levtype=?")
+TYPES = text("select distinct stream, type, levtype from fields")
+PARAMS = text("select distinct param from fields where stream=? and type=? and levtype=?")
 
 
-for n in db.execute(TYPES):
-    print(n)
-    print(stream, type, levtype)
+for stream, type, levtype in db.execute(TYPES):
     print(list(db.execute(PARAMS, stream, type, levtype)))
