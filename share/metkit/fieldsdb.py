06d96d6bc9efa5935b927b2555f476c4df602025
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2018-11-19 15:55:06 +0000

    fieldsdb

--- share/metkit/fieldsdb.py
+++ share/metkit/fieldsdb.py
@@ -4,11 +4,21 @@ db = create_engine("postgres://ecmwf_ro:ecmwf_ro@db-products-dev-00.ecmwf.int/pr
 TYPES = text("select distinct stream, type, levtype from fields")
 PARAMS = text("select distinct param from fields where stream=:stream and type=:type and levtype=:levtype")
 
+P = {}
 
 for stream, type, levtype in db.execute(TYPES):
+    kind = dict(stream=stream, type=type, levtype=levtype)
+    key = (stream, type, levtype)
     print('-'.join([stream, type, levtype]))
-    params = list(db.execute(PARAMS, dict(stream=stream, type=type, levtype=levtype)))
+    params = list(db.execute(PARAMS, kind))
+
     try:
         print("  %s" % sorted([int(x[0]) for x in params]))
+        P[key] = tuple(sorted([int(x[0]) for x in params]))
     except:
         print("  %s" % params,)
+
+
+for k, v in P.items():
+    print(k)
+    print(v)
