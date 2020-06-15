3f38a0c136bd57eb70450f8b8cc44bc06cadd805
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2020-06-15 14:51:00 +0100

    linux compilation

--- src/metkit/codes/Decoder.cc
+++ src/metkit/codes/Decoder.cc
@@ -16,6 +16,7 @@
 
 #include <vector>
 #include <string>
+#include <algorithm>
 
 namespace metkit {
 namespace codes {
--- src/metkit/codes/GRIBDecoder.cc
+++ src/metkit/codes/GRIBDecoder.cc
@@ -8,6 +8,7 @@
  * does it submit to any jurisdiction.
  */
 
+#include <algorithm>
 
 #include <eccodes.h>
 
