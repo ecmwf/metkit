0c35f7804adcf49f3ad4e6344a4f315557c358e4
Emanuele Danovaro <emanuele.danovaro@ecmwf.int>
2021-05-25 12:28:34 +0100

    METK-91 extended set of supported keywords

--- src/tools/compare-mars-requests.py
+++ src/tools/compare-mars-requests.py
@@ -20,9 +20,60 @@ def check_one(name, old, new):
     assert len(old) == 1 and len(new) == 1, f"{name.upper()}: too many values {len(old)} and {len(new)}"
     return old[0], new[0]
 
+def check_atMostOne(name, old, new):
+    assert len(old) < 2 and len(new) < 2, f"{name.upper()}: too many values {len(old)} and {len(new)}"
+    return old[0] if 0 < len(old) else None, new[0] if 0 < len(new) else None
+
 def compare_int(name, old, new):
     for o, n in zip(old, new):
-        assert int(o) == int(n), f"{name.upper()}: value mismatch '{old}' and '{new}'"
+        assert str(o).isnumeric() and str(n).isnumeric() and int(o) == int(n), f"{name.upper()}: value mismatch '{old}' and '{new}'"
+
+def compare_float(name, old, new):
+    for o, n in zip(old, new):
+        assert str(o).isnumeric() and str(n).isnumeric() and float(o) == float(n), f"{name.upper()}: value mismatch '{old}' and '{new}'"
+
+def compare_intAndKeywords(name, old, new, keywords):
+    o, n = check_atMostOne(name, old, new)
+    if o in keywords or str(o).lower() in keywords:
+        o = str(o).lower() if isinstance(o, str) else None
+    if n in keywords or str(n).lower() in keywords:
+        n = str(n).lower() if isinstance(n, str) else None
+    assert o == n or (str(o).isnumeric() and str(n).isnumeric() and int(o) == int(n)), f"{name.upper()}: value mismatch '{old}' and '{new}'"
+
+def compare_grid(name, old, new):
+    for o, n in zip(old, new):
+        try:
+            o = float(o)
+        except ValueError:
+            o = o.lower()
+
+        try:
+            n = float(n)
+        except ValueError:
+            n = n.lower()
+        assert o == n, f"{name.upper()}: value mismatch '{old}' and '{new}'"
+
+def compare_expect(name, old, new):
+    # 'any' or a positive integer value
+    compare_intAndKeywords(name, old, new, ["any"])
+
+def compare_accuracy(name, old, new):
+    # 'off', 'av' or a positive integer value
+    compare_intAndKeywords(name, old, new, [None, "off", "av"])
+
+def compare_frame(name, old, new):
+    # 'off' or a positive integer value
+    compare_intAndKeywords(name, old, new, ["off"])
+
+def compare_truncation(name, old, new):
+    # 'none', 'auto', 'off' or a positive integer value
+    compare_intAndKeywords(name, old, new, ["off", "none", "auto"])
+
+def compare_step(name, old, new):
+    for o, n in zip(old, new):
+        oParts = str(o).split('-')
+        nParts = str(n).split('-')
+        assert len(oParts) == len(nParts) and all(float(oi) == float(ni) for oi, ni in zip(oParts, nParts)), f"{name.upper()}: value mismatch '{old}' and '{new}'"
 
 def compare_str(name, old, new):
     o, n = check_one(name, old, new)
@@ -59,15 +110,29 @@ def compare_date(name, old, new):
     nEnd = parse(str(n), default=datetime.datetime(2001, 12, 31))
     assert oStart.date() == nStart.date() and oEnd.date() == nEnd.date(), f"{name.upper()}: date mismatch '{old[0]}' and '{new[0]}'"
 
+def compare_dates(name, old, new):
+    for o, n in zip(old, new):
+        oStart = parse(str(o), default=datetime.datetime(1901, 1, 1))
+        oEnd = parse(str(o), default=datetime.datetime(2001, 12, 31))
+        nStart = parse(str(n), default=datetime.datetime(1901, 1, 1))
+        nEnd = parse(str(n), default=datetime.datetime(2001, 12, 31))
+        assert oStart.date() == nStart.date() and oEnd.date() == nEnd.date(), f"{name.upper()}: date mismatch '{o}' and '{n}'"
+
 def compare_expver(name, old, new):
     o, n = check_one(name, old, new)
-    if o == n:
-        return
-    if isinstance(o, int) or len(o)<4:
-        o = str(o).zfill(4)
-    if isinstance(n, int) or len(n)<4:
-        n = str(n).zfill(4)
-    assert o == n, f"{name.upper()}: expver mismatch '{old[0]}' and '{new[0]}'"
+    if isinstance(o, str):
+        try:
+            o = int(o)
+        except ValueError:
+            o = o.lower()
+
+    if isinstance(n, str):
+        try:
+            n = int(n)
+        except ValueError:
+            n = n.lower()
+
+    assert o == n, f"{name.upper()}: mismatch '{old[0]}' and '{new[0]}'"
 
 def ignore(name, old, new):
     pass
@@ -76,21 +141,67 @@ def ignore_fixYaml(name, old, new):
     pass
 
 comparators = {
+    "accuracy": ignore_fixYaml, # ?????
+    # "accuracy": compare_accuracy,
+    "anoffset": compare_int, # ?????
+    "area": ignore_fixYaml,
+    # bitmap
+    # block
+    # channel
     "class": compare_str,
-    "stream": compare_str,
-    "type": compare_str,
-    "domain": compare_str,
+    "database": compare_str, # ?????
     "date": compare_date,
+    "diagnostic": compare_int, # ??????
+    "direction": compare_int, # should we handle case "all"
+    "domain": compare_str,
+    "duplicates": compare_str, # can we ignore ?
+    "expect": compare_expect,
     "expver": compare_expver,
-    "levtype": compare_str,
+    "fcmonth": compare_int,
+    "fcperiod": compare_step, # ??????
+    "fieldset": compare_strCaseSensitive,
+    "filter": compare_str,
+    "format": compare_str, # ?????
+    "frame": compare_frame,
+    "frequency": compare_int, # ??????
+    "grid": compare_grid,
+    "hdate": compare_dates,
+    "ident": compare_int,
+    "interpolation": compare_str,
+    # "intgrid":
+    "iteration": compare_int, # ??????
+    "latitude": compare_float,
     "levelist": compare_int,
-    "param": compare_strList,
-    "step": compare_int,
-    "time": compare_time,
-    "target": compare_strCaseSensitive,
-    "accuracy": ignore_fixYaml,
+    "levtype": compare_str,
+    "longitude": compare_float,
+    "lsm": compare_str, # ?????
+    "method": compare_int,
+    "number": compare_int,
+    "obsgroup": compare_str, # ?????
+    "obstype": compare_int,
+    "origin": compare_expver, # ?????
+    "packing": compare_str,
+    "padding": compare_int, # ??????
+    "param": compare_float, # <-- to check that short names have been replaced, otherwise we can use compare_strList
+    "priority": compare_int,
+    "product": compare_str, # ?????
+    "range": compare_int,
+    "refdate": compare_date,
+    "reference": compare_int,
+    "reportype": compare_int,
     "repres": ignore,
-    "area": ignore_fixYaml
+    # resol
+    "rotation": compare_grid,
+    "section": compare_str,
+    "source": compare_strCaseSensitive,
+    "step": compare_step,
+    "stream": compare_str,
+    "system": compare_int,
+    "target": compare_strCaseSensitive,
+    "time": compare_time,
+    "truncation": compare_truncation,
+    "type": compare_str,
+    "use": compare_str
 }
 
 def compare(name, old, new):
