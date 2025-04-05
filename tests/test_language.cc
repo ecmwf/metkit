d1096b3df3b64cac53c0d7f554c39a5596e1637c
Pedro Maciel <Pedro.Maciel@ecmwf.int>
2025-04-05 06:09:02 +0100

    Update GRID keyword: - F, O grids regex correction to trailing digit (+ to *) - F grids allow for arbitrary numbers - N grids removed, those that aren't already assigned to the (C) mars-client - HEALPix grids upcoming syntax (H, Hr, Hn)

---
+++ tests/test_language.cc
@@ -0,0 +1,114 @@
+/*
+ * (C) Copyright 1996- ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation nor
+ * does it submit to any jurisdiction.
+ */
+
+
+#include <map>
+#include <random>
+#include <string>
+#include <vector>
+
+#include "eckit/exception/Exceptions.h"
+#include "eckit/testing/Test.h"
+
+#include "metkit/mars/MarsRequest.h"
+#include "metkit/mars/Type.h"
+
+
+namespace metkit::mars::test {
+
+struct Expected {
+    const std::string verb;
+    const std::map<std::string, std::vector<std::string>> keyvalue;
+};
+
+using Sequence = std::vector<unsigned long>;
+
+Sequence make_random_sequence(Sequence::value_type min, Sequence::value_type max, Sequence::size_type N) {
+    static std::mt19937 gen{std::random_device{}()};
+    static std::uniform_int_distribution<Sequence::value_type> dist(min, max);
+
+    Sequence s(N);
+    std::generate(s.begin(), s.end(), [&] { return dist(gen); });
+    return s;
+}
+
+Sequence make_power_sequence(Sequence::value_type min, Sequence::value_type max) {
+    Sequence s;
+    for (auto i = min; i <= max; i *= 2) {
+        s.push_back(i);
+    }
+    return s;
+}
+
+void expect_mars(const std::string& text, const Expected& expected, bool strict = false) {
+    auto r = MarsRequest::parse(text, strict);
+    EXPECT_EQUAL(expected.verb, r.verb());
+
+    for (const auto& kv : expected.keyvalue) {
+        auto v = r.values(kv.first);
+
+        if (kv.first == "grid") {
+            EXPECT(kv.second.size() == 1 && v.size() == 1 && v[0] == kv.second[0]);
+            continue;
+        }
+
+        EXPECT_EQUAL(kv.second, kv.second);
+    }
+}
+
+// -----------------------------------------------------------------------------
+
+CASE("grid: regular Gaussian grids") {
+    for (const auto& n : make_random_sequence(2, 8000, 20 /*reduced number of tests*/)) {
+        expect_mars("ret, date=-1, grid=" + std::to_string(n), Expected{"retrieve", {{"grid", {std::to_string(n)}}}});
+
+        const Expected expected{"retrieve", {{"grid", {"F" + std::to_string(n)}}}};
+        expect_mars("ret, date=-1, grid=F" + std::to_string(n), expected);
+        expect_mars("ret, date=-1, grid=f" + std::to_string(n), expected);
+    }
+}
+
+// -----------------------------------------------------------------------------
+
+CASE("grid: octahedral Gaussian grids") {
+    for (const auto& n : make_random_sequence(2, 8000, 20 /*reduced number of tests*/)) {
+        const Expected expected{"retrieve", {{"grid", {"O" + std::to_string(n)}}}};
+        expect_mars("ret, date=-1, grid=O" + std::to_string(n), expected);
+        expect_mars("ret, date=-1, grid=o" + std::to_string(n), expected);
+    }
+}
+
+// -----------------------------------------------------------------------------
+
+CASE("grid: reduced classical Gaussian grids") {
+    for (const auto& n : Sequence{32, 48, 64, 80, 96, 128, 160, 200, 256, 320, 400, 512, 640, 800, 1024, 1280, 8000}) {
+        const Expected expected{"retrieve", {{"grid", {"N" + std::to_string(n)}}}};
+        expect_mars("ret, date=-1, grid=N" + std::to_string(n), expected);
+        expect_mars("ret, date=-1, grid=n" + std::to_string(n), expected);
+    }
+}
+
+// -----------------------------------------------------------------------------
+
+CASE("grid: HEALPix grids") {
+    for (const auto& n : make_power_sequence(2, 16384)) {
+        const Expected expected{"retrieve", {{"grid", {"H" + std::to_string(n)}}}};
+        expect_mars("ret, date=-1, grid=H" + std::to_string(n), expected);
+        expect_mars("ret, date=-1, grid=h" + std::to_string(n), expected);
+    }
+}
+
+// -----------------------------------------------------------------------------
+
+}  // namespace metkit::mars::test
+
+int main(int argc, char** argv) {
+    return eckit::testing::run_tests(argc, argv);
+}
