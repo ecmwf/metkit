c1efcca75c8329cc6a730d2e4be67af11d4d1eaf
Kai Kratz <Kai.Kratz@ecmwf.int>
2025-09-05 17:14:31 +0200

    Allow adding custom environment values on DHSProto
    DHSProtocol now allows passing in custom environment values to be sent
    to the server.

--- src/metkit/mars/DHSProtocol.h
+++ src/metkit/mars/DHSProtocol.h
@@ -54,6 +54,8 @@ class DHSProtocol : public BaseProtocol {
 public:
 
     DHSProtocol(const eckit::Configuration&);
+    
+    DHSProtocol(const eckit::Configuration&, const std::map<std::string, std::string>& env);
 
     DHSProtocol(const std::string& name, const std::string& host, int port, bool forewardMessages = false);
 
@@ -80,6 +82,7 @@ private:
     bool error_;
     bool sending_;
     bool forward_;
+    MarsRequest env_;
 
     // -- Methods
     bool wait(eckit::Length&);
