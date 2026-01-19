c1eeded6fb6d9e693ea0499a9b528fd4981dc6a0
Philipp Geier <philipp.geier@ecmwf.int>
2026-01-19 11:35:55 +0100

    Update CodesAPI documentation for length

--- src/metkit/codes/api/CodesAPI.h
+++ src/metkit/codes/api/CodesAPI.h
@@ -196,19 +196,18 @@ public:
     /// @param value Value the field is supposed to be set on.
     virtual void forceSet(const std::string& key, Span<const float> value) = 0;
 
-    /// Returns the number of elements contained for a given key
+    /// Returns the number of elements contained for a given key.
     ///
     /// Can be used to determine if a field is storing a scalar or an array.
     /// @param key Name of the field that is supposed to be inspected.
-    /// @return For given scalars 1 is returned. For given arrays the size of the array is returned..
+    /// @return For given scalars 1 is returned. For given arrays the size of the array is returned.
     virtual size_t size(const std::string& key) const = 0;
 
-    /// Returns the number of chars/bytes contained for a given key
-    //  of type string/byte array.
+    /// Calls `string_length` on the accessor of a field and returns its value.
     ///
-    /// Can be used to determine if a field is storing a scalar or an array.
     /// @param key Name of the field that is supposed to be inspected.
-    /// @return Number of chars/bytes of contained string/byte array
+    /// @return Value of the `string_length` method of the underlying accessor of the key.
+    /// @see ecCodes documentation for more information.
     virtual size_t length(const std::string& key) const = 0;
 
     /// Get the value of the key.
