31b440ec3f7bf380faf5cbce0bfecebd580c1dac
Kevin Nobel <kevin.nobel@ecmwf.int>
2026-02-18 09:44:16 +0000

    Refactor mars2grib template search test

--- tests/mars2grib/backend/mars2grib-test-search.cc
+++ tests/mars2grib/backend/mars2grib-test-search.cc
@@ -1,8 +1,16 @@
-#include <cassert>
+/*
+ * (C) Copyright 2026- ECMWF and individual contributors.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation nor
+ * does it submit to any jurisdiction.
+ */
+
 #include <iostream>
 #include <string>
 #include <tuple>
-#include <utility>
 #include <vector>
 
 // dictionary access traits
@@ -10,6 +18,8 @@
 #include "metkit/mars2grib/utils/dictionary_traits/dictaccess_eckit_configuration.h"
 #include "metkit/mars2grib/utils/dictionary_traits/dictionary_access_traits.h"
 
+#include "eckit/log/Log.h"
+#include "eckit/testing/Test.h"
 #include "metkit/mars2grib/backend/concepts/GeneralRegistry.h"
 #include "metkit/mars2grib/backend/sections/resolver/ActiveConceptsData.h"
 #include "metkit/mars2grib/backend/sections/resolver/ResolvedTemplateData.h"
@@ -21,11 +31,9 @@ using ActiveConceptsData   = metkit::mars2grib::backend::sections::resolver::Act
 using ResolvedTemplateData = metkit::mars2grib::backend::sections::resolver::dsl::ResolvedTemplateData;
 
 std::tuple<ActiveConceptsData, std::size_t> make_ActiveConceptData_from_PayloadEntry(const ResolvedTemplateData& pe) {
-
     using metkit::mars2grib::backend::concepts_::GeneralRegistry;
 
     ActiveConceptsData res{};
-
     res.count = pe.count;
 
     for (std::size_t i = 0; i < res.activeConceptsIndices.size(); ++i) {
@@ -44,140 +52,105 @@ std::tuple<ActiveConceptsData, std::size_t> make_ActiveConceptData_from_PayloadE
     }
 
     return std::tuple{res, pe.templateNumber};
-};
+}
 
-int main() {
 
+CASE("Section 0") {
     using metkit::mars2grib::backend::sections::resolver::SectionLayoutData;
     using metkit::mars2grib::backend::sections::resolver::SectionTemplateSelector;
-    using metkit::mars2grib::backend::sections::resolver::debug::debug_print_ActiveConceptsData;
-    using metkit::mars2grib::backend::sections::resolver::debug::debug_print_SectionLayoutData;
-    using metkit::mars2grib::backend::sections::resolver::detail::CompressionMask;
-    using metkit::mars2grib::backend::sections::resolver::detail::make_CompressionMask_or_throw;
-    using metkit::mars2grib::backend::sections::resolver::dsl::ResolvedTemplateData;
-
     using metkit::mars2grib::frontend::resolution::recipes::impl::Section0Recipes;
+
+    const auto payload  = Section0Recipes.getPayload();
+    const auto selector = SectionTemplateSelector::make(Section0Recipes);
+    for (std::size_t i = 0; i < payload.size(); ++i) {
+        auto [activeConcepts, expectedTemplateNumber] = make_ActiveConceptData_from_PayloadEntry(payload[i]);
+        const auto& sectionLayoutData                 = selector.select_or_throw(activeConcepts);
+        eckit::Log::error() << "Section 0 :: " << i << " expected template number " << expectedTemplateNumber
+                            << " but found " << sectionLayoutData.templateNumber << std::endl;
+        ASSERT(expectedTemplateNumber == sectionLayoutData.templateNumber);
+    }
+}
+
+CASE("Section 1") {
+    using metkit::mars2grib::backend::sections::resolver::SectionLayoutData;
+    using metkit::mars2grib::backend::sections::resolver::SectionTemplateSelector;
     using metkit::mars2grib::frontend::resolution::recipes::impl::Section1Recipes;
+
+    const auto payload  = Section1Recipes.getPayload();
+    const auto selector = SectionTemplateSelector::make(Section1Recipes);
+    for (std::size_t i = 0; i < payload.size(); ++i) {
+        auto [activeConcepts, expectedTemplateNumber] = make_ActiveConceptData_from_PayloadEntry(payload[i]);
+        const auto& sectionLayoutData                 = selector.select_or_throw(activeConcepts);
+        eckit::Log::error() << "Section 1 :: " << i << " expected template number " << expectedTemplateNumber
+                            << " but found " << sectionLayoutData.templateNumber << std::endl;
+        ASSERT(expectedTemplateNumber == sectionLayoutData.templateNumber);
+    }
+}
+
+CASE("Section 2") {
+    using metkit::mars2grib::backend::sections::resolver::SectionLayoutData;
+    using metkit::mars2grib::backend::sections::resolver::SectionTemplateSelector;
     using metkit::mars2grib::frontend::resolution::recipes::impl::Section2Recipes;
+
+    const auto payload  = Section2Recipes.getPayload();
+    const auto selector = SectionTemplateSelector::make(Section2Recipes);
+    for (std::size_t i = 0; i < payload.size(); ++i) {
+        auto [activeConcepts, expectedTemplateNumber] = make_ActiveConceptData_from_PayloadEntry(payload[i]);
+        const auto& sectionLayoutData                 = selector.select_or_throw(activeConcepts);
+        eckit::Log::error() << "Section 2 :: " << i << " expected template number " << expectedTemplateNumber
+                            << " but found " << sectionLayoutData.templateNumber << std::endl;
+        ASSERT(expectedTemplateNumber == sectionLayoutData.templateNumber);
+    }
+}
+
+CASE("Section 3") {
+    using metkit::mars2grib::backend::sections::resolver::SectionLayoutData;
+    using metkit::mars2grib::backend::sections::resolver::SectionTemplateSelector;
     using metkit::mars2grib::frontend::resolution::recipes::impl::Section3Recipes;
+
+    const auto payload  = Section3Recipes.getPayload();
+    const auto selector = SectionTemplateSelector::make(Section3Recipes);
+    for (std::size_t i = 0; i < payload.size(); ++i) {
+        auto [activeConcepts, expectedTemplateNumber] = make_ActiveConceptData_from_PayloadEntry(payload[i]);
+        const auto& sectionLayoutData                 = selector.select_or_throw(activeConcepts);
+        eckit::Log::error() << "Section 3 :: " << i << " expected template number " << expectedTemplateNumber
+                            << " but found " << sectionLayoutData.templateNumber << std::endl;
+        ASSERT(expectedTemplateNumber == sectionLayoutData.templateNumber);
+    }
+}
+
+CASE("Section 4") {
+    using metkit::mars2grib::backend::sections::resolver::SectionLayoutData;
+    using metkit::mars2grib::backend::sections::resolver::SectionTemplateSelector;
     using metkit::mars2grib::frontend::resolution::recipes::impl::Section4Recipes;
+
+    const auto payload  = Section4Recipes.getPayload();
+    const auto selector = SectionTemplateSelector::make(Section4Recipes);
+    for (std::size_t i = 0; i < payload.size(); ++i) {
+        auto [activeConcepts, expectedTemplateNumber] = make_ActiveConceptData_from_PayloadEntry(payload[i]);
+        const auto& sectionLayoutData                 = selector.select_or_throw(activeConcepts);
+        eckit::Log::error() << "Section 4 :: " << i << " expected template number " << expectedTemplateNumber
+                            << " but found " << sectionLayoutData.templateNumber << std::endl;
+        ASSERT(expectedTemplateNumber == sectionLayoutData.templateNumber);
+    }
+}
+
+CASE("Section 5") {
+    using metkit::mars2grib::backend::sections::resolver::SectionLayoutData;
+    using metkit::mars2grib::backend::sections::resolver::SectionTemplateSelector;
     using metkit::mars2grib::frontend::resolution::recipes::impl::Section5Recipes;
 
-    const auto payload0 = Section0Recipes.getPayload();
-    const auto payload1 = Section1Recipes.getPayload();
-    const auto payload2 = Section2Recipes.getPayload();
-    const auto payload3 = Section3Recipes.getPayload();
-    const auto payload4 = Section4Recipes.getPayload();
-    const auto payload5 = Section5Recipes.getPayload();
-
-    {
-        const SectionTemplateSelector selector = SectionTemplateSelector::make(Section0Recipes);
-
-        for (std::size_t i = 0; i < payload0.size(); ++i) {
-            auto [activeConcepts, expectedTemplateNumber] = make_ActiveConceptData_from_PayloadEntry(payload0[i]);
-
-            // debug_print_ActiveConceptsData( activeConcepts, "ACTIVE-DATA", std::cout );
-
-            const SectionLayoutData& sectionLayoutData = selector.select_or_throw(activeConcepts);
-
-            // debug_print_SectionLayoutData( sectionLayoutData, "SECTION-LAYOUT", std::cout );
-
-            std::cout << "Section0 :: " << i << " - " << expectedTemplateNumber << " - "
-                      << sectionLayoutData.templateNumber << std::endl;
-            assert(expectedTemplateNumber == sectionLayoutData.templateNumber);
-        }
+    const auto payload  = Section5Recipes.getPayload();
+    const auto selector = SectionTemplateSelector::make(Section5Recipes);
+    for (std::size_t i = 0; i < payload.size(); ++i) {
+        auto [activeConcepts, expectedTemplateNumber] = make_ActiveConceptData_from_PayloadEntry(payload[i]);
+        const auto& sectionLayoutData                 = selector.select_or_throw(activeConcepts);
+        eckit::Log::error() << "Section 5 :: " << i << " expected template number " << expectedTemplateNumber
+                            << " but found " << sectionLayoutData.templateNumber << std::endl;
+        ASSERT(expectedTemplateNumber == sectionLayoutData.templateNumber);
     }
+}
 
-    {
-        const SectionTemplateSelector selector = SectionTemplateSelector::make(Section1Recipes);
-
-        for (std::size_t i = 0; i < payload1.size(); ++i) {
-            auto [activeConcepts, expectedTemplateNumber] = make_ActiveConceptData_from_PayloadEntry(payload1[i]);
-
-            // debug_print_ActiveConceptsData( activeConcepts, "ACTIVE-DATA", std::cout );
-
-            const SectionLayoutData& sectionLayoutData = selector.select_or_throw(activeConcepts);
-
-            // debug_print_SectionLayoutData( sectionLayoutData, "SECTION-LAYOUT", std::cout );
-
-            std::cout << "Section1 :: " << i << " - " << expectedTemplateNumber << " - "
-                      << sectionLayoutData.templateNumber << std::endl;
-            assert(expectedTemplateNumber == sectionLayoutData.templateNumber);
-        }
-    }
-
-    {
-        const SectionTemplateSelector selector = SectionTemplateSelector::make(Section2Recipes);
-
-        for (std::size_t i = 0; i < payload2.size(); ++i) {
-            auto [activeConcepts, expectedTemplateNumber] = make_ActiveConceptData_from_PayloadEntry(payload2[i]);
-
-            // debug_print_ActiveConceptsData( activeConcepts, "ACTIVE-DATA", std::cout );
-
-            const SectionLayoutData& sectionLayoutData = selector.select_or_throw(activeConcepts);
-
-            // debug_print_SectionLayoutData( sectionLayoutData, "SECTION-LAYOUT", std::cout );
-
-            std::cout << "Section2 :: " << i << " - " << expectedTemplateNumber << " - "
-                      << sectionLayoutData.templateNumber << std::endl;
-            assert(expectedTemplateNumber == sectionLayoutData.templateNumber);
-        }
-    }
-
-    {
-        const SectionTemplateSelector selector = SectionTemplateSelector::make(Section3Recipes);
-
-        for (std::size_t i = 0; i < payload3.size(); ++i) {
-            auto [activeConcepts, expectedTemplateNumber] = make_ActiveConceptData_from_PayloadEntry(payload3[i]);
-
-            // debug_print_ActiveConceptsData( activeConcepts, "ACTIVE-DATA", std::cout );
-
-            const SectionLayoutData& sectionLayoutData = selector.select_or_throw(activeConcepts);
-
-            // debug_print_SectionLayoutData( sectionLayoutData, "SECTION-LAYOUT", std::cout );
-
-            std::cout << "Section3 :: " << i << " - " << expectedTemplateNumber << " - "
-                      << sectionLayoutData.templateNumber << std::endl;
-            assert(expectedTemplateNumber == sectionLayoutData.templateNumber);
-        }
-    }
-
-    {
-        const SectionTemplateSelector selector = SectionTemplateSelector::make(Section4Recipes);
-
-        for (std::size_t i = 0; i < payload4.size(); ++i) {
-            auto [activeConcepts, expectedTemplateNumber] = make_ActiveConceptData_from_PayloadEntry(payload4[i]);
-
-            // debug_print_ActiveConceptsData( activeConcepts, "ACTIVE-DATA", std::cout );
-
-            const SectionLayoutData& sectionLayoutData = selector.select_or_throw(activeConcepts);
-
-            // debug_print_SectionLayoutData( sectionLayoutData, "SECTION-LAYOUT", std::cout );
-
-            std::cout << "Section4 :: " << i << " - " << expectedTemplateNumber << " - "
-                      << sectionLayoutData.templateNumber << std::endl;
-            assert(expectedTemplateNumber == sectionLayoutData.templateNumber);
-        }
-    }
-
-    {
-        const SectionTemplateSelector selector = SectionTemplateSelector::make(Section5Recipes);
-
-        for (std::size_t i = 0; i < payload5.size(); ++i) {
-            auto [activeConcepts, expectedTemplateNumber] = make_ActiveConceptData_from_PayloadEntry(payload5[i]);
-
-            // debug_print_ActiveConceptsData( activeConcepts, "ACTIVE-DATA", std::cout );
-
-            const SectionLayoutData& sectionLayoutData = selector.select_or_throw(activeConcepts);
-
-            // debug_print_SectionLayoutData( sectionLayoutData, "SECTION-LAYOUT", std::cout );
-
-            std::cout << "Section5 :: " << i << " - " << expectedTemplateNumber << " - "
-                      << sectionLayoutData.templateNumber << std::endl;
-            assert(expectedTemplateNumber == sectionLayoutData.templateNumber);
-        }
-    }
-
-    // Exit point
-    return 0;
-};
+int main(int argc, char** argv) {
+    return eckit::testing::run_tests(argc, argv);
+}
