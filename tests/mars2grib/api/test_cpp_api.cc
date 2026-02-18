1dbd4ed8fee6a7c419ac16715dcebd29bd38776a
Kevin Nobel <kevin.nobel@ecmwf.int>
2026-02-18 09:43:11 +0000

    Print exceptions in mars2grib CPP API test

--- tests/mars2grib/api/test_cpp_api.cc
+++ tests/mars2grib/api/test_cpp_api.cc
@@ -8,36 +8,43 @@
  * does it submit to any jurisdiction.
  */
 
+#include <exception>
 #include "eckit/config/LocalConfiguration.h"
+#include "eckit/log/CodeLocation.h"
 #include "eckit/testing/Test.h"
 #include "metkit/mars2grib/api/Mars2Grib.h"
-
-namespace metkit::test {
+#include "metkit/mars2grib/utils/mars2gribExceptions.h"
 
 CASE("mars2grib_api") {
-    auto encoder = mars2grib::Mars2Grib();
+    try {
 
-    eckit::LocalConfiguration mars;
-    mars.set("origin", "ecmf");
-    mars.set("class", "od");
-    mars.set("stream", "oper");
-    mars.set("type", "fc");
-    mars.set("expver", "0001");
-    mars.set("grid", "N200");
-    mars.set("packing", "ccsds");
-    mars.set("param", 130);
-    mars.set("levtype", "hl");
-    mars.set("levelist", 2);
-    mars.set("date", 2026'02'05);
-    mars.set("time", 00'00'00);
-    mars.set("step", 0);
+        auto encoder = metkit::mars2grib::Mars2Grib();
 
-    std::vector<double> vals(200, 237.15);
+        eckit::LocalConfiguration mars;
+        mars.set("origin", "ecmf");
+        mars.set("class", "od");
+        mars.set("stream", "oper");
+        mars.set("type", "fc");
+        mars.set("expver", "0001");
+        mars.set("grid", "N200");
+        mars.set("packing", "ccsds");
+        mars.set("param", 130);
+        mars.set("levtype", "hl");
+        mars.set("levelist", 2);
+        mars.set("date", 2026'02'05);
+        mars.set("time", 00'00'00);
+        mars.set("step", 0);
 
-    encoder.encode(vals, mars);
+        std::vector<double> vals(200, 237.15);
+
+        encoder.encode(vals, mars);
+    }
+    catch (const std::exception& e) {
+        metkit::mars2grib::utils::exceptions::printExceptionStack(e, eckit::Log::error());
+        std::throw_with_nested(
+            metkit::mars2grib::utils::exceptions::Mars2GribGenericException("CPP API test failed", Here()));
+    }
 }
-
-}  // namespace metkit::test
 
 int main(int argc, char** argv) {
     return eckit::testing::run_tests(argc, argv);
