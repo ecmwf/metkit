9bffc96de942a4e9c04c2e267c6b8dca1bc4a9d3
Mirco Valentini <mirco.valentini@ecmwf.int>
2026-05-26 15:24:11 +0000

    Fix multiple bugs for encoding of BrightnessTemperature

--- src/metkit/mars2grib/backend/concepts/derived/derivedMatcher.h
+++ src/metkit/mars2grib/backend/concepts/derived/derivedMatcher.h
@@ -13,7 +13,9 @@ namespace metkit::mars2grib::backend::concepts_ {
 
 template <class MarsDict_t, class OptDict_t>
 std::size_t derivedMatcher(const MarsDict_t& mars, const OptDict_t& opt) {
+
     using metkit::mars2grib::utils::dict_traits::get_or_throw;
+    using metkit::mars2grib::utils::dict_traits::has;
 
     const auto& type = get_or_throw<std::string>(mars, "type");
     if (type == "em" ||    // Ensemble mean
@@ -25,6 +27,11 @@ std::size_t derivedMatcher(const MarsDict_t& mars, const OptDict_t& opt) {
         type == "sot"      // Shift of tails
     ) {
         return static_cast<std::size_t>(DerivedType::Default);
+    }
+
+    if (has(mars, "channel") && has(mars, "param") && get_or_throw<long>(mars, "param") == 194 && has(mars, "stream") &&
+        get_or_throw<std::string>(mars, "stream") == "elda") {
+        return static_cast<std::size_t>(DerivedType::BrightnessTemperature);
     }
 
     return compile_time_registry_engine::MISSING;
