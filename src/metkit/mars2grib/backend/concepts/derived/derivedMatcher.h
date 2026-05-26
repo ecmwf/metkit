15cacced14745114557aecfaf4b7b4fdae44b79e
Mirco Valentini <mirco.valentini@ecmwf.int>
2026-05-26 15:24:11 +0000

    mars2grib: Fix encoding of BrightnessTemperature

--- src/metkit/mars2grib/backend/concepts/derived/derivedMatcher.h
+++ src/metkit/mars2grib/backend/concepts/derived/derivedMatcher.h
@@ -13,11 +13,12 @@ namespace metkit::mars2grib::backend::concepts_ {
 
 template <class MarsDict_t, class OptDict_t>
 std::size_t derivedMatcher(const MarsDict_t& mars, const OptDict_t& opt) {
+
     using metkit::mars2grib::utils::dict_traits::get_or_throw;
+    using metkit::mars2grib::utils::dict_traits::has;
 
     const auto& type = get_or_throw<std::string>(mars, "type");
-    if (type == "em" ||    // Ensemble mean
-        type == "es" ||    // Ensemble standard deviation
+    if (type == "es" ||    // Ensemble standard deviation
         type == "ses" ||   // Ensemble spread of estimation
         type == "taem" ||  // Time-averaged ensemble mean
         type == "taes" ||  // Time-averaged ensemble standard deviation
@@ -25,6 +26,16 @@ std::size_t derivedMatcher(const MarsDict_t& mars, const OptDict_t& opt) {
         type == "sot"      // Shift of tails
     ) {
         return static_cast<std::size_t>(DerivedType::Default);
+    }
+
+    if (type == "em") {  // Ensemble mean (special handling for brightness temperature products)
+        if (has(mars, "channel") && has(mars, "param") && get_or_throw<long>(mars, "param") == 194 &&
+            has(mars, "stream") && get_or_throw<std::string>(mars, "stream") == "elda") {
+            return static_cast<std::size_t>(DerivedType::BrightnessTemperatureEnsembleMean);
+        }
+        else {
+            return static_cast<std::size_t>(DerivedType::Default);
+        }
     }
 
     return compile_time_registry_engine::MISSING;
