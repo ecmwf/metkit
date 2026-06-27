f6c4ffb28b4ff1bae874d8483388c9e0ee3a45eb
Mirco Valentini <mirco.valentini@ecmwf.int>
2026-06-27 02:18:59 +0000

    mars2grib: cosmetic improvements

--- src/metkit/mars2grib/backend/concepts/satellite/satelliteEncoding.h
+++ src/metkit/mars2grib/backend/concepts/satellite/satelliteEncoding.h
@@ -216,10 +216,10 @@ void SatelliteOp(const MarsDict_t& mars, const ParDict_t& par, const OptDict_t& 
             if constexpr (Section == SecLocalUseSection && Stage == StagePreset) {
 
                 // Check/Validation
-                validation::match_LocalDefinitionNumber_or_throw(opt, out, {24,37});
+                validation::match_LocalDefinitionNumber_or_throw(opt, out, {24, 37});
 
                 // Deductions
-                long channelNumber       = deductions::resolve_Channel_or_throw(mars, par, opt);
+                long channelNumber = deductions::resolve_Channel_or_throw(mars, par, opt);
                 // long numberOfFrequencies = deductions::resolve_NumberOfFrequencies_or_throw(mars, par, opt);
 
                 // Encoding
