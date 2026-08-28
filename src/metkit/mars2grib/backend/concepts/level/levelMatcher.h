81ac6e92689ff8496166c3d6ed269e845fc2aa48
Charles Pelletier <charles.pelletier@ecmwf.int>
2026-08-28 10:02:23 +0000

    Add all statistics for two extra snow params (228038 and 238)

--- src/metkit/mars2grib/backend/concepts/level/levelMatcher.h
+++ src/metkit/mars2grib/backend/concepts/level/levelMatcher.h
@@ -238,7 +238,8 @@ inline std::size_t matchSOL(const long param) {
     if (matchAny(param, 262000, 262024)) {
         return static_cast<std::size_t>(LevelType::SeaIceLayer);
     }
-    if (matchAny(param, 33, 74, 238, 228038, 235406, 228141, 235078, 235080, 235238, 237080, 238080, 239080)) {
+    if (matchAny(param, 33, 74, 238, 228038, 235406, 237406, 238406, 239406, 228141, 235078, 237078, 238078, 239078,
+                 235080, 237080, 238080, 239080, 235238, 237238, 238238, 239238)) {
         return static_cast<std::size_t>(LevelType::SnowLayer);
     }
     if (matchAny(param, 183, 235077, 260199, 260360)) {
